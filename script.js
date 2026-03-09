70a6858f2bc1c0d8badc61e36475e8a4c433b00c
Raksha.J <jobi41215@gmail.com>
2026-03-09 23:36:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -44,6 +44,31 @@ result = "Suggested Career: Data Analyst";
 else{
 result = "Suggested Career: Teacher";
 }
+function saveProfile(){
+
+let name = document.getElementById("name").value;
+let email = document.getElementById("email").value;
+let interest = document.getElementById("interest").value;
+
+localStorage.setItem("studentName", name);
+localStorage.setItem("studentEmail", email);
+localStorage.setItem("studentInterest", interest);
+
+alert("Profile saved successfully!");
+
+}
+
+function showProfile(){
+
+let name = localStorage.getItem("studentName");
+let email = localStorage.getItem("studentEmail");
+let interest = localStorage.getItem("studentInterest");
+
+document.getElementById("displayName").innerText = "Name: " + name;
+document.getElementById("displayEmail").innerText = "Email: " + email;
+document.getElementById("displayInterest").innerText = "Interest: " + interest;
+
+}
 
 document.getElementById("result").innerText = result;
 
