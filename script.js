e0080196f666f493e4b685aac612d2a8cfc9511a
Raksha.J <jobi41215@gmail.com>
2026-03-09 22:52:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ function developer(){
 alert("Suggested Career: Software Developer");
 }
 
+function teacher(){
+alert("Suggested Career: Teacher");
+}
+
 function designer(){
 alert("Suggested Career: Graphic Designer");
 }
