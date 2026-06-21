03fbacf89d1051011cf2336932e69b09b7b46a0a
Raksha.J <jobi41215@gmail.com>
2026-06-22 00:41:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -795,3 +795,18 @@ result
 );
 
 }
+function showSignup(){
+
+document.getElementById("signupForm").style.display = "block";
+
+document.getElementById("loginForm").style.display = "none";
+
+}
+
+function showLogin(){
+
+document.getElementById("signupForm").style.display = "none";
+
+document.getElementById("loginForm").style.display = "block";
+
+}
